923e6350025bd65223fe72876c6be957e5af6b0f
2571964s <2571964s@student.gla.ac.uk>
2023-03-14 15:11:21 +0000

    Files list get sorted accourding to file size now - Close #1

--- conventer.py
+++ conventer.py
@@ -5,9 +5,14 @@ import sys
 # get all .mp4 files in the 'files' folder and put them in a list:
 files_paths = [os.path.join('files', file) for file in os.listdir('files') if file.endswith('.mp4')]
 
-print('Files to found:')
-for file in files_paths:
+# sort the list according to file size (from small to large)
+files_paths.sort(key=os.path.getsize)
+
+print('Files to found (sorted according to file size):')
+      for file in files_paths:
+    # print filename and size:
     print(f'    {file}')
+
 
 for input_file_path in files_paths:
     # output file name is the same as input file name with .mp3 extension
